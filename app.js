0a719de8e64bba3a967875e3769cf6c8cdf92f5d
bielcarpi <biel.carpi@students.salle.url.edu>
2021-11-05 13:12:39 +0100

    Add app.js config (express)

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+//TODO: Change src folder to dist folder before production
+var filesPath = 'src';
+
+
+//Serving static content with express middleware (dist directory)
+app.use(express.static(filesPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(filePath + '/index.html');
+});
+
+app.get('/todo-form.html', (req, res) => {
+    res.sendFile(filePath + '/todo-form.html');
+});
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+}); 
